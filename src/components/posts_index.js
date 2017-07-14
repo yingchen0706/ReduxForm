93ca46f6a7a9b1721c3cb560bfad4ce1662f11d9
yingchen0706 <yingchen0706@gmail.com>
2017-07-14 17:02:26 +0800

    update

---
+++ src/components/posts_index.js
@@ -0,0 +1,11 @@
+import React, {Component} from 'react';
+
+export default class PostsIndex extends Component {
+    render() {
+        return (
+            <div>
+                Post Index
+            </div>
+        )
+    }
+}
