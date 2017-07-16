8df311494b8a220bbd3bf11a26465191d7cd184d
yingchen0706 <yingchen0706@gmail.com>
2017-07-17 00:34:30 +0800

    124-127

--- webpack.config.js
+++ webpack.config.js
@@ -9,14 +9,18 @@ module.exports = {
   },
   module: {
     loaders: [{
-      test: /\.js$/,
-      exclude: /(node_modules|bower_components)/,
-      use: {
-        loader: 'babel-loader'
+      exclude: /node_modules/,
+      loader: 'babel',
+      query: {
+        presets: ['react', 'es2015', 'stage-1']
       }
     }]
   },
+  resolve: {
+    extensions: ['', '.js', '.jsx']
+  },
   devServer: {
     historyApiFallback: true,
+    contentBase: './'
   }
 };
